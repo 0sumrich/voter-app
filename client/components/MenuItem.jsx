7682b039d64a76d62cfc921260e82efe1f5ae14f
Glitch (spring-parade) <none>
2018-03-18 19:16:18 +0000

    💉💩 Checkpoint ./client/components/MenuItem.jsx:692249/27 ./client/components/Menu.jsx:692249/70

--- client/components/MenuItem.jsx
+++ client/components/MenuItem.jsx
@@ -3,7 +3,7 @@ const React=require('react');
 function MenuItem(props){
   const style = {
     float: props.float,
-    visibility: props.showMenu ? "initial" : "hidden"
+    display: props.showMenu ? "initial" : "none"
   }
   return <li className={props.className} style={style} onMouseOver={props.handleMouseOver} onMouseOut={props.handleMouseOut}>{props.content}</li>;
 }
