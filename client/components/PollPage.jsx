467f67c6af1275d17631d868fe301472f688d80c
Glitch (spring-parade) <none>
2018-04-07 17:00:29 +0000

    🌦🐙 Checkpoint ./client/components/PollPage.jsx:692249/1062

--- client/components/PollPage.jsx
+++ client/components/PollPage.jsx
@@ -2,7 +2,7 @@ const React=require('react'),
       HomeButton = require('../components/HomeButton'),
       Redirect = require('react-router-dom').Redirect;
 
-/*
+
 function PollPage(props){  
   const ID = props.match.params.id,
         data = props.polls,
@@ -15,39 +15,6 @@ function PollPage(props){
     </div>;
   
   return pollpage;
-}
-*/
-class PollPage extends React.Component {
-  constructor(props){
-    super(props);
-    this.state={loaded: false, polls: []}
-  }
-  
-  componentWillMount(){
-    fetch('/api/polls').then(res => res.json()).then(data => {      
-      //let result = [];
-      //data.map(i => i.forEach(p => result.push(p)));      
-      this.setState({polls: data.sort((a, b) => new Date(b.date) - new Date(a.date)), ID:this.props.match.params.id});
-    });
-  }
-      
-   
-  render(){
-    const ID = this.state.id,
-          data = this.state.polls,
-          poll = data.filter(o => o._id==ID)[0],
-          title = data.length>0 ? poll.title : "";
-    
-    console.log(data, poll);
-    
-    const pollpage = 
-      <div style={{maxWidth: 800, margin: 'auto'}}>
-        <h4 style={{padding: '0px 15px'}}>{title}</h4>
-        <HomeButton />
-      </div>;
-
-    return pollpage;
-  }
 }
 
 module.exports=PollPage;
--- public/bundle.js
+++ public/bundle.js
@@ -74199,52 +74199,19 @@ const React=__webpack_require__(0),
       HomeButton = __webpack_require__(234),
       Redirect = __webpack_require__(20).Redirect;
 
-/*
+
 function PollPage(props){  
   const ID = props.match.params.id,
         data = props.polls,
         poll = data.filter(o => o._id==ID)[0];
 
   const pollpage = 
-    <div style={{maxWidth: 800, margin: 'auto'}}>
-      <h4 style={{padding: '0px 15px'}}>{ID}</h4>
-      <HomeButton />
-    </div>;
+    React.createElement("div", {style: {maxWidth: 800, margin: 'auto'}}, 
+      React.createElement("h4", {style: {padding: '0px 15px'}}, ID), 
+      React.createElement(HomeButton, null)
+    );
   
   return pollpage;
-}
-*/
-class PollPage extends React.Component {
-  constructor(props){
-    super(props);
-    this.state={loaded: false, polls: []}
-  }
-  
-  componentWillMount(){
-    fetch('/api/polls').then(res => res.json()).then(data => {      
-      //let result = [];
-      //data.map(i => i.forEach(p => result.push(p)));      
-      this.setState({polls: data.sort((a, b) => new Date(b.date) - new Date(a.date)), ID:this.props.match.params.id});
-    });
-  }
-      
-   
-  render(){
-    const ID = this.state.id,
-          data = this.state.polls,
-          poll = data.filter(o => o._id==ID)[0],
-          title = data.length>0 ? poll.title : "";
-    
-    console.log(data, poll);
-    
-    const pollpage = 
-      React.createElement("div", {style: {maxWidth: 800, margin: 'auto'}}, 
-        React.createElement("h4", {style: {padding: '0px 15px'}}, title), 
-        React.createElement(HomeButton, null)
-      );
-
-    return pollpage;
-  }
 }
 
 module.exports=PollPage;
