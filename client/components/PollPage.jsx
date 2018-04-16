83368b0737c131d107dcb256e324220f23455f8d
Glitch (spring-parade) <none>
2018-04-16 07:53:32 +0000

    🌼🐯 Checkpoint ./client/components/PollPage.jsx:692249/2037

--- client/components/PollPage.jsx
+++ client/components/PollPage.jsx
@@ -43,7 +43,57 @@ function PollPage(props){
 
 class PollPage extends React.Component {
   constructor(props) {
-    poll: {}
+    super(props)
+    this.state={
+      polls: []
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    console.log(e.target.value);
+  }
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.polls
+  }
+  componentDidMount() {
+    const data = this.props.polls;    
+    this.setState({polls: data});
+  }
+  
+  render(){
+    if(this.state.data.length<1) {
+    return <div></div>
+  } else {
+    const ID = props.match.params.id,
+          data = props.polls,
+          poll = data.filter(o => o._id==ID)[0],
+          CHOICES = poll.choices.map(o => o.choice);
+    
+    console.log(CHOICES);
+    
+    props.handleFormSubmit(poll);
+    
+    const Choice = ({choice}) => (
+      <div style={{padding: '0px 15px'}}>
+        <input type="radio" value={choice} name='choice' onChange={e=>console.log(e.target.value)}/>
+        <span style={{marginLeft: 15}}>{choice}</span>
+      </div>
+    );
+
+    const pollpage = 
+      <div style={{width: '100%', maxWidth: 800, margin: 'auto'}}>
+        <div style={{display:'block', maxWidth: 400, margin: 'auto', border: '1px solid #e5e5e5', borderRadius: 5}}>
+          <h4 style={{padding: 15, margin: 0, background: '#e5e5e5'}}>{poll.title}</h4>
+          <div style={{padding: '15px 0'}}>
+            <form>
+              {CHOICES.map(c => <Choice choice={c} />)}
+            </form>
+          </div>
+        </div>
+        <HomeButton />
+      </div>;
+
+    return pollpage;
   }
 }
 
