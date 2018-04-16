22d5119694ebb5cafc4f037f97d06b2444a78d62
Glitch (spring-parade) <none>
2018-04-16 12:03:21 +0000

    🚧🌛 Checkpoint ./package.json:692249/3021 ./client/components/Poll.jsx:692249/812

--- client/components/Poll.jsx
+++ client/components/Poll.jsx
@@ -5,7 +5,42 @@ const React = require('react'),
       Button = ReactBootstrap.Button,
       ReactTooltip = require('react-tooltip'),
       Redirect = require('react-router-dom').Redirect,
-      chart = require('chart.js');
+      ReactChartJs = require('react-chartjs-2'),
+      Bar = ReactChartJs.HorizontalBar;
+
+/*
+const data = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'My First dataset',
+      backgroundColor: 'rgba(255,99,132,0.2)',
+      borderColor: 'rgba(255,99,132,1)',
+      borderWidth: 1,
+      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+      hoverBorderColor: 'rgba(255,99,132,1)',
+      data: [65, 59, 80, 81, 56, 55, 40]
+    }
+  ]
+};
+
+export default React.createClass({
+  displayName: 'BarExample',
+
+  render() {
+    return (
+      <div>
+        <h2>Horizontal Bar Example</h2>
+        <HorizontalBar data={data} />
+      </div>
+    );
+  }
+});
+*/
+
+const Chart = ({props}) => {
+  
+}
 
 class Poll extends React.Component {
   constructor(props){
