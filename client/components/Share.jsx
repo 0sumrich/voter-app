c8c67b2f100e7cc47baafe3fcb8772653d305c76
Glitch (spring-parade) <none>
2018-05-09 09:54:38 +0000

    📰🎽 Checkpoint ./client/components/Share.jsx:692249/508

--- client/components/Share.jsx
+++ client/components/Share.jsx
@@ -22,6 +22,7 @@ class Share extends React.Component {
     this.state={
       mouseover: false
     }
+    this.shareIcon = React.createRef();
     this.handleOver=this.handleOver.bind(this);
     this.handleRemClick=this.handleRemClick.bind(this);
   }
@@ -33,37 +34,41 @@ class Share extends React.Component {
     document.getElementById('share').close();
   }
   
+  handleIconOver(){
+    console.log(this
+  }
+  
   render() {
     const tweetTxt = 'text=Check out my poll',
       url = '&url=https://spring-parade.glitch.me/home/poll/'+this.props.id,
       href = 'https://twitter.com/intent/tweet?'+tweetTxt+url,
       newUrl='https://spring-parade.glitch.me/home/poll/'+this.props.id;
-
+  //onClick={this.handleRemClick}
     const style={display: 'inline-block', padding: 5};
     const dialog = (
       <dialog id='share' style={{border: 'none', boxShadow: '10px 5px 5px grey'}}>
           <div style={{margin: 'auto'}}>
             <p style={{display: 'inline'}}>Share</p>
-            <div className="pull-right" style={{color: '#e5e5e5'}} onClick={this.handleRemClick}>
+            <div className="pull-right" style={{color: '#e5e5e5', cursor: 'pointer'}} onClick={this.handleRemClick}>
               <Glyph glyph="remove" />
             </div><br/>
             <div>              
-              <div style={style}>
+              <div style={style} ref={this.shareIcon}>
                 <FacebookShareButton url={newUrl}><FacebookIcon size={25} round /></FacebookShareButton>
               </div>
-              <div style={style}>
+              <div style={style} ref={this.shareIcon}>
                 <GooglePlusShareButton url={newUrl}><GooglePlusIcon size={25} round /></GooglePlusShareButton>
               </div>
-              <div style={style}>
+              <div style={style} ref={this.shareIcon}>
                 <TwitterShareButton url={newUrl}><TwitterIcon size={25} round /></TwitterShareButton>
               </div>
-              <div style={style}>
+              <div style={style} ref={this.shareIcon}>
                 <RedditShareButton url={newUrl}><RedditIcon size={25} round /></RedditShareButton>
               </div>
-              <div style={style}>
+              <div style={style} ref={this.shareIcon}>
                 <TumblrShareButton url={newUrl}><TumblrIcon size={25} round /></TumblrShareButton>
               </div>
-              <div style={style}>
+              <div style={style} ref={this.shareIcon}>
                 <EmailShareButton url={newUrl}><EmailIcon size={25} round /></EmailShareButton>
               </div>
             </div>
