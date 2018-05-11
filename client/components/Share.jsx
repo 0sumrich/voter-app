40c1e2856d17ab84a5224fd89c43b0ea390ff83b
Glitch (spring-parade) <none>
2018-05-11 12:46:20 +0000

    👕🤘 Checkpoint ./client/components/Share.jsx:692249/2480 ./package.json:692249/66

--- client/components/Share.jsx
+++ client/components/Share.jsx
@@ -31,10 +31,8 @@ class Modal extends React.Component {
   componentDidMount() {
     this.el.id="modal";
     this.el.style.position="absolute";
-    this.el.style.top="0px";
-    this.el.style.display="block";
-    this.el.style.width="100%";
-    this.el.style.margin="auto";
+    this.el.style.top=0+window.scrollY + "px";
+    this.el.style.width="100vw";
     document.body.appendChild(this.el);
   }
 
@@ -93,29 +91,14 @@ const ShareRow = ({style, url}) => {
   )
 };
 
-/*
-const dialog = (
-      <dialog id='share' open style={{border: 'none', boxShadow: '10px 5px 5px grey'}}>
-          <div style={{margin: 'auto'}}>
-            <p style={{display: 'inline'}}>Share</p>
-            <div className="pull-right" style={{color: '#e5e5e5', cursor: 'pointer'}} onClick={this.handleRemClick}>
-              <Glyph glyph="remove" />
-            </div><br/>
-            <ShareRow style={style} url={newUrl} />
-          </div>
-        </dialog>        
-    );
-*/
-
-
 class Dialog extends React.Component {
   render() {
     const {handleClick, style, url, id} = this.props;
-    const gRect = document.getElementById(id).getBoundingClientRect();
-    const pRect = document.getElementsByClassName('polls')[0].getBoundingClientRect();
-    const pos = 0-pRect.height+pRect.top-83;
+    //const gRect = document.getElementById(id).getBoundingClientRect();
+    //const pRect = document.getElementsByClassName('polls')[0].getBoundingClientRect();
+    //const pos = 0-pRect.height+pRect.top-83;
     return (
-      <dialog id='share' open style={{border: 'none', boxShadow: '10px 5px 5px grey', position: 'relative', top: gRect.top+window.scrollY+"px"}}>
+      <dialog id='share' open style={{border: 'none', boxShadow: '10px 5px 5px grey', position: 'relative', top: "50vh"}}>
           <div style={{margin: 'auto'}}>
             <p style={{display: 'inline'}}>Share</p>
             <div className="pull-right" style={{color: '#e5e5e5', cursor: 'pointer'}} onClick={handleClick}>
@@ -153,20 +136,8 @@ class Share extends React.Component {
       url = '&url=https://spring-parade.glitch.me/home/poll/'+this.props.id,
       href = 'https://twitter.com/intent/tweet?'+tweetTxt+url,
       newUrl='https://spring-parade.glitch.me/home/poll/'+this.props.id;
-  //onClick={this.handleRemClick}
-    const style={display: 'inline-block', padding: 5};
-    const dialog = (
-      <dialog id='share' open style={{border: 'none', boxShadow: '10px 5px 5px grey'}}>
-          <div style={{margin: 'auto'}}>
-            <p style={{display: 'inline'}}>Share</p>
-            <div className="pull-right" style={{color: '#e5e5e5', cursor: 'pointer'}} onClick={this.handleRemClick}>
-              <Glyph glyph="remove" />
-            </div><br/>
-            <ShareRow style={style} url={newUrl} />
-          </div>
-        </dialog>        
-    );
-    //const share =  <p style={{margin: 0, fontSize: '1em', cursor: 'pointer'}} onClick={this.handleClick}>Share</p>;
+  
+    const style={display: 'inline-block', padding: 5};    
     const Popup = () =>{
       if(this.state.clicked) {              
         return <Modal><Dialog handleClick={this.handleRemClick} style={style} url={newUrl} id={this.props.id} /></Modal>
@@ -183,52 +154,5 @@ class Share extends React.Component {
     ) 
   }
 }
-
-/*
-const ShareRow = ({style, handleOver, url}) => {
-  const Fb = () => (
-    <div style={style} onMouseover={handleOver}
-      <FacebookShareButton url={url}><FacebookIcon size={25} round /></FacebookShareButton>
-    </div>
-  );
-  const Google = () => (
-    <div style={style} >
-      <GooglePlusShareButton url={url}><GooglePlusIcon size={25} round /></GooglePlusShareButton>
-    </div>
-  );
-  const Twitter = () => (
-    <div style={style} >
-      <TwitterShareButton url={url}><TwitterIcon size={25} round /></TwitterShareButton>
-    </div>
-  );
-  const Reddit = () => (
-    <div style={style} ref={this.shareIcon}>
-      <RedditShareButton url={url}><RedditIcon size={25} round /></RedditShareButton>
-    </div>
-  );
-  const Tumblr = () => (
-    <div style={style} ref={this.shareIcon}>
-      <TumblrShareButton url={url}><TumblrIcon size={25} round /></TumblrShareButton>
-    </div>
-  );
-  const Email = () => (
-    <div style={style} ref={this.shareIcon}>
-      <EmailShareButton url={newUrl}><EmailIcon size={25} round /></EmailShareButton>
-    </div>
-  )
-  return (
-    <div>
-      <Fb />
-      <Google />
-      <Twitter />
-      <Reddit />
-      <Tumblr />
-      <Email />
-    </div>
-  )
-}
-
-*/
-
 
 module.exports=Share;
