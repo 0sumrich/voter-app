808d91df8fe82fe339aff828be09a4f08e6d7966
Glitch (spring-parade) <none>
2018-05-12 09:21:07 +0000

    🍩🦄 Checkpoint ./.glitch-assets:692249/413 ./client/components/App.jsx:692249/296

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -17,7 +17,10 @@ const {FacebookIcon} = require('react-share');
 
 const FBButton = ({ onClick }) => (
   <div onClick={onClick}>
-      <FacebookIcon color={'#4568AF'} size={22} />
+      <img 
+        style={{display: 'inline', height: 25}}
+        src="https://cdn.glitch.com/aca77c25-fdc0-472e-9f9c-d3bc89eb95a1%2Ff-ogo_RGB_HEX-58.svg?1526116424343"/>
+      <span style={{marginLeft: 5}}>Facebook</span>
   </div>
 );
 
